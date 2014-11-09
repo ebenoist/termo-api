d4e3e0ab7bfaa879f85661e4454c610df37ea108
Erik Benoist <ebenoist@gmail.com>
2014-11-08 19:43:02 -0600

    panic if api key does not exist

--- main.go
+++ main.go
@@ -33,27 +33,25 @@ func buildThermostat() *Thermostat {
 
 func main() {
 	log.Println("Booting termo!")
-
-	if os.Getenv("API_KEY") != "" {
-		log.Printf("Using key %s", os.Getenv("API_KEY"))
-
-		thermostat := buildThermostat()
-
-		// CLEAN UP
-		c := make(chan os.Signal, 1)
-		signal.Notify(c, os.Interrupt)
-		go func() {
-			for sig := range c {
-				log.Printf("Caught %v", sig)
-				log.Println("Exiting Termo!!")
-				thermostat.Stop()
-				os.Exit(1)
-			}
-		}()
-
-		go thermostat.Run()
-		apiRun(thermostat)
-	} else {
-		log.Println("Could not find API_KEY")
+	if os.Getenv("API_KEY") == "" {
+		panic("Could not find API_KEY")
 	}
+
+	log.Printf("Using key %s", os.Getenv("API_KEY"))
+	thermostat := buildThermostat()
+
+	// CLEAN UP
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			log.Printf("Caught %v", sig)
+			log.Println("Exiting Termo!!")
+			thermostat.Stop()
+			os.Exit(1)
+		}
+	}()
+
+	go thermostat.Run()
+	apiRun(thermostat)
 }
